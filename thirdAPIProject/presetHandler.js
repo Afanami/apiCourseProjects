b1411bb4d1799820efda53b8b2caa2a0b5a84dcb
Afanami <afan6333@gmail.com>
2018-06-28 17:02:07 +1200

    finish beat mixer

--- thirdAPIProject/presetHandler.js
+++ thirdAPIProject/presetHandler.js
@@ -7,10 +7,10 @@ const presetHandler = (requestType, index, newPresetArray) => {
 
     if(requestType === 'PUT' || requestType === 'GET') {
         if (index > 15 || index < 0) {
-            response.push('404');
+            response.push(404);
             return response;
         } else {
-            response.push('200');
+            response.push(200);
             if (requestType === 'GET') {
                 response.push(presets[index]);
                 return response;
@@ -21,12 +21,12 @@ const presetHandler = (requestType, index, newPresetArray) => {
             }
         }   
     } else {
-        response.push ('400');
+        response.push (400);
         return response;
     }
 };
 
-console.log(presets);
-console.log(presetHandler('GET', 18, []))
+// console.log(presets);
+// console.log(presetHandler('GET', 18, []))
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
--- thirdAPIProject/public/js/script.js
+++ thirdAPIProject/public/js/script.js
@@ -92,3 +92,54 @@ const invert = (arrayName) => {
 // clear('hiHats');
 // clear('rideCymbals');
 // console.log('New Kicks Array: ' + kicks + '\n\nNew Snares Array: ' + snares + '\n\nNew HiHats Array: ' + hiHats + '\n\nNew RideCymbals Array: ' + rideCymbals);
+
+const getNeighborPads = (x, y, size) => {
+    let neighbors =[];
+
+    if (x > size-1 || y > size-1 || x < 0 || y < 0) {
+        return neighbors = [];
+    }
+
+    // Bottom left corner [up, right]
+    if (x === 0 && y === 0) {
+        return neighbors = [[x, y+1],[x+1, y]];
+    }
+
+    // Top left corner [down, right]
+    if (x === 0 && y === size-1) {
+        return neighbors = [[x, y-1], [x+1, y]];
+    }
+
+    // Bottom right corner [up, left]
+    if (x === size-1 && y === 0) {
+        return neighbors = [[x, y+1], [x-1, y]];
+    }
+
+    // Top right corner [down, left]
+    if (x === size-1 && y === size-1) {
+        return neighbors = [[x, y-1], [x-1, y]];
+    }
+
+    // Bottom edge but not corner [up, left, right];
+    if (y === 0 && (x !== size-1 && x !== 0)) {
+        return neighbors = [[x, y+1], [x-1, y], [x+1, y]];
+    }
+
+    // Left edge but not corner [up, down, right]
+    if (x === 0 && (y !== size-1 && y !== 0)) {
+        return neighbors = [[x, y+1], [x, y-1], [x+1, y]];
+    }
+
+    // Top edge but not corner [down, left, right]
+    if (y === size-1 && (x !== size-1 && x !== 0)) {
+        return neighbors = [[x, y-1], [x-1, y], [x+1, y]];
+    }
+
+    // Right edge but not corner [up, down, left]
+    if (x === size-1 && (y !== size-1 && y !== 0)) {
+        return neighbors = [[x, y+1], [x, y-1], [x-1, y]];
+    }
+
+    // Return all four neighbors [up, down, left, right]
+    return neighbors = [[x, y+1], [x, y-1], [x-1, y], [x+1, y]];
+}
