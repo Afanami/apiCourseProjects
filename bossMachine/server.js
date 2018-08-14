36bf2390120d68b6e85c6a6ed5f8d62051cac4c8
Afanami <afan6333@gmail.com>
2018-08-15 10:30:18 +1200

    setup basic routing and required all necessary modules

--- bossMachine/server.js
+++ bossMachine/server.js
@@ -1,7 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 module.exports = app;
+
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 /* Do not change the following line! It is required for testing and allowing
 *  the frontend application to interact as planned with the api server
@@ -9,17 +12,20 @@ module.exports = app;
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
-
+app.use(cors());
 
 // Add middware for parsing request bodies here:
-
+app.use(bodyParser.json());
 
 // Mount your existing apiRouter below at the '/api' path.
-const apiRouter = require('./server/api');
+const apiRouter = require("./server/api");
 
+app.use("/api", apiRouter);
 
 // This conditional is here for testing purposes:
-if (!module.parent) { 
+if (!module.parent) {
   // Add your code to start the server listening at PORT below:
-
+  app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`);
+  });
 }
--- bossMachine/server/api.js
+++ bossMachine/server/api.js
@@ -1,6 +1,9 @@
-const express = require('express');
+const db = require("./db.js");
+const express = require("express");
 const apiRouter = express.Router();
 
-
+apiRouter.delete("/meetings", (res, req, next) => {
+  db.deleteAllFromDatabase("meetings");
+});
 
 module.exports = apiRouter;
