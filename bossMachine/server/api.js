a4ce85e60287e00e6bf303117e4481f25d340384
Afanami <afan6333@gmail.com>
2018-08-17 15:39:23 +1200

    added functionality to get and delete routes for minions, ideas and meetings

--- bossMachine/server/api.js
+++ bossMachine/server/api.js
@@ -9,13 +9,20 @@ const apiRouter = express.Router();
 /* @ROUTE /api/minions */
 /* =================== */
 
-// Get an array of all minions
+// Check if minion exists and return an array of all minions
 apiRouter.get("/minions", (req, res, next) => {
+  const minions = db.getAllFromDatabase("minions");
+  minions !== null ? res.status(200).send(minions) : res.status(404);
   next();
 });
 
 // Create a new minion and save it to the database
 apiRouter.post("/minions", (req, res, next) => {
+  // console.log(req);
+  // let minions = db.getAllFromDatabase("minions");
+  // db.addToDatabase("minions", minions) !== null
+  //   ? res.status(201).send(db.addToDatabase("minions", minions))
+  //   : res.status(404);
   next();
 });
 
@@ -25,16 +32,27 @@ apiRouter.post("/minions", (req, res, next) => {
 
 // Get a single minion by id
 apiRouter.get("/minions/:minionId", (req, res, next) => {
+  const minionId = req.params.minionId;
+  const minionAtId = db.getFromDatabaseById("minions", minionId);
+  isNaN(Number(minionId)) || minionAtId === null || !minionAtId
+    ? res.status(404)
+    : res.status(200).send(minionAtId);
   next();
 });
 
 // Update a single minion by id
 apiRouter.put("/minions/:minionId", (req, res, next) => {
+  console.log(req);
   next();
 });
 
 // Delete a single minion by id
 apiRouter.delete("/minions/:minionId", (req, res, next) => {
+  const minionId = req.params.minionId;
+  const deletedMinion = db.deleteFromDatabasebyId("minions", minionId);
+  isNaN(Number(minionId)) || deletedMinion === null || !deletedMinion
+    ? res.status(404)
+    : res.status(204).send(deletedMinion);
   next();
 });
 
@@ -70,6 +88,8 @@ apiRouter.delete("/minions/:minionId", (req, res, next) => {
 
 // Get an array of all ideas
 apiRouter.get("/ideas", (req, res, next) => {
+  const ideas = db.getAllFromDatabase("ideas");
+  ideas !== null ? res.status(200).send(ideas) : res.status(404);
   next();
 });
 
@@ -83,17 +103,27 @@ apiRouter.post("/ideas", (req, res, next) => {
 /* ========================= */
 
 // Get a single idea by id
-apiRouter.get("/ideas/ideaId", (req, res, next) => {
+apiRouter.get("/ideas/:ideaId", (req, res, next) => {
+  const ideaId = req.params.ideaId;
+  const ideaAtId = db.getFromDatabaseById("ideas", ideaId);
+  isNaN(Number(ideaId)) || ideaAtId === null || !ideaAtId
+    ? res.status(404)
+    : res.status(200).send(ideaAtId);
   next();
 });
 
 // Update a single idea by id
-apiRouter.put("/ideas/ideaId", (req, res, next) => {
+apiRouter.put("/ideas/:ideaId", (req, res, next) => {
   next();
 });
 
 // Delete a single idea by id
-apiRouter.delete("/ideas/ideaId", (req, res, next) => {
+apiRouter.delete("/ideas/:ideaId", (req, res, next) => {
+  const ideaId = req.params.ideaId;
+  const deletedIdea = db.deleteFromDatabasebyId("ideas", ideaId);
+  isNaN(Number(ideaId)) || deletedIdea === null || !deletedIdea
+    ? res.status(404)
+    : res.status(204).send(deletedIdea);
   next();
 });
 
@@ -103,6 +133,8 @@ apiRouter.delete("/ideas/ideaId", (req, res, next) => {
 
 // Get an array of all meetings
 apiRouter.get("/meetings", (req, res, next) => {
+  const meetings = db.getAllFromDatabase("meetings");
+  meetings !== null ? res.status(200).send(meetings) : res.status(404);
   next();
 });
 
@@ -111,12 +143,16 @@ apiRouter.get("/meetings", (req, res, next) => {
 automatically by the server upon request. Use the provided createMeeting function exported frmo db.js to create a new meeting object. */
 // Create a new meeting and save it to the database
 apiRouter.post("/meetings", (req, res, next) => {
+  const newMeeting = db.createMeeting();
+  const addMeeting = db.addToDatabase("meetings", newMeeting);
+  addMeeting ? res.status(201).send(addMeeting) : res.status(404);
   next();
 });
 
 // Create a new meeting and save it to the database
 apiRouter.delete("/meetings", (req, res, next) => {
-  db.deleteAllFromDatabase("meetings");
+  const deleteMeetings = db.deleteAllFromDatabase("meetings");
+  res.status(204).send(deleteMeetings);
 });
 
 module.exports = apiRouter;
