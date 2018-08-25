72a7583f4c9e9c60d1abf363d93960d287e6f621
Afanami <afan6333@gmail.com>
2018-08-25 22:29:37 +1200

    updated dependency and start creating sql tables

---
+++ xPressProject/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database(
+  process.env.TEST_DATABASE || "./database.sqlite"
+);
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on server port ${PORT}`);
+});
+
+module.exports = app;
