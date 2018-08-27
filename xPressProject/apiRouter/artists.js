f5daad9f014ad7ae59699697710cd61406a4d88e
Afanami <afan6333@gmail.com>
2018-08-27 21:43:18 +1200

    completed routing for updating and deleting artists

--- xPressProject/apiRouter/artists.js
+++ xPressProject/apiRouter/artists.js
@@ -43,9 +43,9 @@ artistsRouter.post("/", (req, res, next) => {
     isCurrentlyEmployed = 1;
   }
 
-  console.log(
-    `Name is ${name}, \nDOB is ${dateOfBirth}, \nBiography is ${biography}, \nEmployment Status is ${isCurrentlyEmployed}`
-  );
+  // console.log(
+  //   `Name is ${name}, \nDOB is ${dateOfBirth}, \nBiography is ${biography}, \nEmployment Status is ${isCurrentlyEmployed}`
+  // );
 
   db.run(
     "INSERT INTO Artist (name, date_of_birth, biography, is_currently_employed) VALUES ($name, $dob, $bio, $employed)",
@@ -104,8 +104,77 @@ artistsRouter.get("/:artistId", (req, res, next) => {
   res.status(200).json({ artist: req.artist });
 });
 
-// app.get("/:artistId", (req, res, next) => {});
-// app.put("/:artistId", (req, res, next) => {});
-// app.delete("/:artistId", (req, res, next) => {});
+// Update artist after param route checks validity of artist
+artistsRouter.put("/:artistId", (req, res, next) => {
+  let name = req.body.artist.name;
+  let dateOfBirth = req.body.artist.dateOfBirth;
+  let biography = req.body.artist.biography;
+
+  // Send 400 error if required body values missing
+  if (!name || !dateOfBirth || !biography) {
+    res.sendStatus(400);
+  }
+
+  let isCurrentlyEmployed = req.body.artist.isCurrentlyEmployed;
+  // Set employment status if does not exist
+  if (!isCurrentlyEmployed) {
+    isCurrentlyEmployed = 1;
+  }
+
+  // Update artist based on id
+  db.run(
+    "UPDATE Artist SET name = $name, date_of_birth = $dob, biography = $bio, is_currently_employed = $employed WHERE Artist.id = $artistId",
+    {
+      $name: name,
+      $dob: dateOfBirth,
+      $bio: biography,
+      $employed: isCurrentlyEmployed,
+      $artistId: req.params.artistId
+    },
+    err => {
+      if (err) {
+        next(err);
+      } else {
+        // Get updated artist and send as json
+        db.get(
+          "SELECT * FROM Artist WHERE Artist.id = $artistId",
+          {
+            $artistId: req.params.artistId
+          },
+          (err, artist) => {
+            res.status(200).json({ artist });
+          }
+        );
+      }
+    }
+  );
+});
+
+// Delete artist by setting employment to 0
+artistsRouter.delete("/:artistId", (req, res, next) => {
+  // Set employed to 0 to artist based on id
+  db.run(
+    "UPDATE Artist SET is_currently_employed = 0 WHERE Artist.id = $artistId",
+    {
+      $artistId: req.params.artistId
+    },
+    err => {
+      if (err) {
+        next(err);
+      } else {
+        // Get updated artist and send as json
+        db.get(
+          "SELECT * FROM Artist WHERE Artist.id = $artistId",
+          {
+            $artistId: req.params.artistId
+          },
+          (err, artist) => {
+            res.status(200).json({ artist });
+          }
+        );
+      }
+    }
+  );
+});
 
 module.exports = artistsRouter;
